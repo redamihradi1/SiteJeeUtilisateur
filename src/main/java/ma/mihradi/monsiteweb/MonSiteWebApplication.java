8a680fe498adfa3c02cb6f1ceec8845ea2fc7553
Reda Mihradi <mihradi_reda@emsi-edu.ma>
2022-03-27 18:59:58 +0100

    Ajout de 3 users

--- src/main/java/ma/mihradi/monsiteweb/MonSiteWebApplication.java
+++ src/main/java/ma/mihradi/monsiteweb/MonSiteWebApplication.java
@@ -1,7 +1,11 @@
 package ma.mihradi.monsiteweb;
 
+import ma.mihradi.monsiteweb.entities.Utilisateur;
+import ma.mihradi.monsiteweb.repositories.UtilisateurRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MonSiteWebApplication {
@@ -9,5 +13,22 @@ public class MonSiteWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(MonSiteWebApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner commandLineRunner(UtilisateurRepository utilisateurRepository){
+        return args -> {
+            Utilisateur u1 = new Utilisateur(null,"reda","reda","mihradi","reda",2000.0,23);
+            Utilisateur u2 = new Utilisateur(null,"rim","rim","mihradi","rim",2000.0,18);
+            Utilisateur u3 = new Utilisateur(null,"rayan","rim","mihradi","rim",2000.0,15);
+            utilisateurRepository.save(u1);
+            utilisateurRepository.save(u2);
+            utilisateurRepository.save(u3);
+
+
+
+
+
+        };}
+
 
 }
--- src/main/java/ma/mihradi/monsiteweb/entities/Utilisateur.java
+++ src/main/java/ma/mihradi/monsiteweb/entities/Utilisateur.java
@@ -16,7 +16,7 @@ import javax.persistence.Id;
 @AllArgsConstructor
 public class Utilisateur {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
     private String nomUtilisateur;
     private String motDePasse;
     private String nom;
